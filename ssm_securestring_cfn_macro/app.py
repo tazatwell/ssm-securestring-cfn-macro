b6335e961b5ab8897884d4f8e435b79ef80203f6
tazatwell <tazatwell@gmail.com>
2022-12-12 16:16:39 -0800

    Refactored app code to put put parameter args handling in test handler file. Using a function to handle logic instead of seperate class. Added init file to allow pylinting. Added .pylintrc file

--- ssm_securestring_cfn_macro/app.py
+++ ssm_securestring_cfn_macro/app.py
@@ -1,52 +1,111 @@
-import json
+"""
+ssm_securestring_cfn_macro
+    Lambda Application Code to create AWS SSM SecureString Parameters using a CloudFormation Macro
+        uses boto3 library for aws api
+        uses cfnresponse library to send status response object
+        uses included put_parameter_args to unit test handling input to aws ssm api call
+
+"""
+
 import cfnresponse
 import boto3
-import put_parameter_args as param_args
-# import requests
-
 
 def lambda_handler(event, context):
+    """Lambda Handler entrypoint for lambda"""
 
     print(event)
 
     print(context)
 
-    if 'ResourceProperties' not in event:
-      cfnresponse.send(event, context, cfnresponse.FAILED, {'Reason': "No Resource Properties key in Request"})
+    validate, validateError = validate_request(event)
+    if validate == False:
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.FAILED,
+            {'Reason': f"{validateError}"}
+        )
 
-    if 'Name' not in event['ResourceProperties']:
-      cfnresponse.send(event, context, cfnresponse.FAILED, {'Reason': "No Name key in Request"})
+    if event['RequestType'] == "Create" or event['RequestType'] == "Update":
+        ssm_client = boto3.client("ssm")
+        put_parameter_args = put_param_args(event['ResourceProperties'])
 
-    if 'Value' not in event['ResourceProperties']:
-      cfnresponse.send(event, context, cfnresponse.FAILED, {'Reason': "No Value key in Request"})
+        ssm_client.put_parameter(put_parameter_args.construct_put_parameter_args())
 
-    if 'Type' not in event['ResourceProperties'] and event['RequestType'] == "Create":
-      cfnresponse.send(event, context, cfnresponse.FAILED, {'Reason': "Type key must be specified when creating an SSM Parameter"}) 
-
-    if event['RequestType'] == 'Create':
-      ssm_client = boto3.client('ssm')
-      put_parameter_args = param_args(event['ResourceProperties'])
-
-      ssm_client.put_parameter(put_parameter_args.construct_parameter_args())
-
-      cfnresponse.send(event, context, cfnresponse.SUCCESS, "Created parameter. Arguments: {}".format(put_parameter_args.construct_parameter_args))
-
-    elif event['RequestType'] == 'Update':
-      ssm_client = boto3.client('ssm')
-      put_parameter_args = param_args(event['ResourceProperties'])
-
-      ssm_client.put_parameter(put_parameter_args.construct_parameter_args())
-
-      cfnresponse.send(event, context, cfnresponse.SUCCESS, "Updated parameter. Arguments: {}".format(put_parameter_args.construct_parameter_args))
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.SUCCESS,
+            f"Created parameter. Arguments: {put_parameter_args.construct_put_parameter_args()}"
+        )
 
     elif event['RequestType'] == 'Delete':
-      # delete SSM Param here
-      ssm_client = boto3.client('ssm')
+        # delete SSM Param here
+        ssm_client = boto3.client('ssm')
 
-      ssm_client.delete_parameter(Name=event['ResourceProperties']['Name'])
+        ssm_client.delete_parameter(Name=event['ResourceProperties']['Name'])
 
-      cfnresponse.send(event, context, cfnresponse.SUCCESS, "Deleted parameter {}".format(event['ResourceProperties']['Name']))
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.SUCCESS,
+            f"Deleted parameter {event['ResourceProperties']['Name']}"
+        )
 
     else:
-      cfnresponse.send(event, context, cfnresponse.FAILED, "Input event has invalid RequestType field: " + event['RequestType'])
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.FAILED,
+            f"Input event has invalid RequestType field: {event['RequestType']}"
+        )
 
+def validate_request(event):
+    """Perform high level validation based on put_parameter args"""
+
+    if 'ResourceProperties' not in event:
+        return (False, "No Resource Properties key in request")
+
+    if 'Name' not in event['ResourceProperties']:
+        return (False, "No Name key in request's ResourceProperties field")
+
+    if 'Value' not in event['ResourceProperties']:
+        return (False, "No Value key in request's ResourceProperties field")
+
+    if 'Type' not in event['ResourceProperties'] and event['RequestType'] == "Create":
+        return (False, "No Type key in request's ResourceProperties field. Type must be specified when creating an SSM Parameter")
+
+    return True, None
+
+def construct_put_parameter_args(eventProps):
+    """
+        Construct args passed to ssm.put_parameter_args
+        Handles dynamic event properties
+    """
+
+    put_parameter_args = {}
+    put_parameter_args['Name'] = eventProps['Name']
+    put_parameter_args['Value'] = eventProps['Value']
+
+    if 'Description' in eventProps:
+        put_parameter_args['Description'] = eventProps['Description']
+
+    if 'Type' in eventProps:
+        put_parameter_args['Type'] = eventProps['Type']
+
+    if 'KeyId' in eventProps:
+        put_parameter_args['KeyId'] = eventProps['KeyId']
+    if 'Overwrite' in eventProps:
+        put_parameter_args['Overwrite'] = eventProps['Overwrite']
+    if 'AllowedPattern' in eventProps:
+        put_parameter_args['AllowedPattern'] = eventProps['AllowedPattern']
+    if 'Tags' in eventProps:
+        put_parameter_args['Tags'] = eventProps['Tags']
+    if 'Tier' in eventProps:
+        put_parameter_args['Tier'] = eventProps['Tier']
+    if 'Policies' in eventProps:
+        put_parameter_args['Policies'] = eventProps['Policies']
+    if 'DataType' in eventProps:
+        put_parameter_args['DataType'] = eventProps['DataType']
+
+    return put_parameter_args
